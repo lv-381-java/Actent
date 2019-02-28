71e866c2a57434826bd9cb661a2fcc707bd4af96
onlyVone <43787744+onlyVone@users.noreply.github.com>
2019-02-28 20:53:09 +0200

    Create Chat entity

---
+++ src/main/java/com/softserve/actent/model/entity/Chat.java
@@ -0,0 +1,39 @@
+package com.softserve.actent.model.entity;
+
+import com.softserve.actent.constant.StringConstants;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.NonNull;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+@Data
+@NoArgsConstructor
+@Entity
+@Table(name = "chats")
+public class Chat {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NonNull
+    @OneToMany(fetch = FetchType.LAZY,
+            mappedBy = "chat")
+    private List<Message> messages;
+
+    @NonNull
+    @NotNull(message = StringConstants.CHAT_TYPE_NOT_BE_BLANK)
+    @Enumerated(EnumType.ORDINAL)
+    private ChatType type;
+
+    @NonNull
+    @OneToMany(fetch=FetchType.LAZY,
+            mappedBy = "id")
+    private Set<User> banned = new HashSet<>();
+
+}
