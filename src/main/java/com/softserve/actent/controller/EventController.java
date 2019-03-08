8f57d4686c09a975326f37537133695c98a807d4
Oleh Volchkov <volchkov.oleh@gmail.com>
2019-03-08 17:06:01 +0200

    newline

--- src/main/java/com/softserve/actent/controller/EventController.java
+++ src/main/java/com/softserve/actent/controller/EventController.java
@@ -17,6 +17,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class EventController {
 
     private final EventService eventService;
+    
     private final EventConverter eventConverter;
 
     @Autowired
