4bf743fe8907896cfbfd017cfca15456de59b880
onlyVone <43787744+onlyVone@users.noreply.github.com>
2019-04-11 16:39:42 +0300

    Create count of message dto

---
+++ src/main/java/com/softserve/actent/model/dto/chat/ChatCountOfMessagesDto.java
@@ -0,0 +1,11 @@
+package com.softserve.actent.model.dto.chat;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+public class ChatCountOfMessagesDto {
+
+    Long countOfMessages;
+}
