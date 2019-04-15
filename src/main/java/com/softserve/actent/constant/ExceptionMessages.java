924cd8eb5799ad7c0c49b565fbbe49968d064e9f
Ira <ira.shturyn@gmail.com>
2019-04-16 01:27:13 +0300

    exception message

--- src/main/java/com/softserve/actent/constant/ExceptionMessages.java
+++ src/main/java/com/softserve/actent/constant/ExceptionMessages.java
@@ -35,7 +35,6 @@ public class ExceptionMessages {
     public static final String REVIEW_INAPPROPRIATE_AUTHOR_ID = "Author id should be number greater than zero.";
 
     public static final String IMAGE_NO_ID = "You need to provide image id.";
-    public static final String IMAGE_NO_HASH = "You need to provide image hash.";
     public static final String IMAGE_NO_FILEPATH = "You need to provide image url.";
     public static final String IMAGE_INAPPROPRIATE_HASH_LENGTH = "SHA256 hash must be exactly 64 symbols in length.";
     public static final String IMAGE_NOT_FOUND_WITH_ID = "Image with requested id was not found.";
@@ -52,7 +51,7 @@ public class ExceptionMessages {
 
     public static final String CHAT_BY_THIS_ID_IS_NOT_FOUND = "Chat by this id is not found";
     public static final String ACTIVE_BY_THIS_TYPE_IS_NOT_FOUND = "While adding chat to active. Active by this type is not found";
-  
+
     public static final String YOU_CAN_NOT_CHANGE_THIS_MESSAGE = "You can't change this message";
     public static final String MESSAGE_NOT_FOUND = "Message not found";
 
@@ -61,22 +60,8 @@ public class ExceptionMessages {
     public static final String EMAIL_ALREADY_USED = "There is a user with such email!";
     public static final String USER_NOT_REGISTRED = "User not registered!";
 
-    public static final String COUNTRY_NOT_FOUND = "Country with this id not found";
-    public static final String REGION_NOT_FOUND = "Region with this id not found";
-    public static final String CITY_NOT_FOUND = "City with this id not found";
     public static final String LOCATION_NOT_FOUND = "Location with this id not found";
-  
-    public static final String COUNTRY_ALREADY_EXIST = "Country with this name already exist in base";
-    public static final String REGION_ALREADY_EXIST = "Region with this name in this country already exist in base";
-    public static final String CITY_ALREADY_EXIST = "City with this name in this region already exist in base";
-    public static final String LOCATION_ALREADY_EXIST = "Location with this name in this city already exist in base";
+    public static final String LOCATION_ADDRESS_NOT_FOUND = "Location with this address not found";
 
-    public static final String NO_COUNTRIES_IN_BASE = "There are no countries in base";
-    public static final String NO_REGIONS_IN_BASE = "There are no regions in base";
-    public static final String NO_REGIONS_IN_THIS_COUNTRY_IN_BASE = "There are no regions in this country in base";
-    public static final String NO_CITIES_IN_BASE = "There are no cities in base";
-    public static final String NO_CITIES_IN_THIS_REGION_IN_BASE = "There are no cities in this region in base";
-    public static final String NO_LOCATIONS_IN_BASE = "There are no locations in base";
-    public static final String NO_LOCATIONS_IN_THIS_CITY_IN_BASE = "There are no locations in this city in base";
     public static final String USER_BY_THIS_LOGIN_IS_NOT_FOUND = "User by this login is not found.";
 }
