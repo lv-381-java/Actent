74305520bd85b277a92ec76c7cd9563c86e22937
Ira <ira.shturyn@gmail.com>
2019-04-18 16:57:09 +0300

    add LocationCoordinatesDto

---
+++ src/main/java/com/softserve/actent/model/dto/location/LocationCoordinatesDto.java
@@ -0,0 +1,11 @@
+package com.softserve.actent.model.dto.location;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+public class LocationCoordinatesDto {
+    private Double latitude;
+    private Double longtitude;
+}
