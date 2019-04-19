0a8815eb9bda02f809ae375ac9ad7e6380e9edac
Ira <ira.shturyn@gmail.com>
2019-04-19 13:56:20 +0300

    add fix location

--- actent-app-client/src/component/createevent/Location.js
+++ actent-app-client/src/component/createevent/Location.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import Select from 'react-select';
 import axios from 'axios';
+import {MDBIcon} from "mdbreact";
+import Link from '@material-ui/core/Link';
 
 export default class Location
     extends React.Component {
     state = {
-        locations: []
+        locations: [],
+        address: ""
     };
 
     componentDidMount() {
@@ -30,10 +33,10 @@ export default class Location
     };
 
     handleChange = name => value => {
-        // this.setState({
-        //     address: value.value,
-        //     isSet: true
-        // });
+        this.setState({
+            address: value.value,
+            isSet: true
+        });
         this.props.setAddress(value.value)
 
     };
@@ -43,23 +46,19 @@ export default class Location
             this.props.setAddress(value)
         }
 
-        // this.setState({address: value}, () => {
-        //     this.getLocations()
-        // });
+        this.setState({address: value}, () => {
+            this.getLocations()
+        });
 
     };
 
 
     handleAddLocation = () => {
-
         if (this.props.address && this.props.address.length > 0) {
-
             let url = `http://localhost:8080/api/v1/locations/byAddress/${this.props.address}`;
-
             axios.get(url)
                 .then(response => {
                     const savedId = response.data;
-
                     this.props.setLocationId(savedId.id);
                 })
                 .catch(function (error) {
@@ -81,20 +80,27 @@ export default class Location
         return (
             <div className="form-group">
                 <label>Location</label>
-                <div className="selectStyle">
-                    <Select
+                <div className="selectStyle address">
+                    {
+                        this.props.address !== ""
+                        &&(<Link rel="noopener"
+                                 target="_blank"
+                                 href={`https://www.google.com/maps/place/${this.props.address}`}>
+                            <MDBIcon icon="map" size="2x"/>
+                        </Link>)
+                    }
+                    <Select className="find_addres_fild"
                         options={this.state.locations.map(location => ({
                             value: location.address,
                             label: location.address
                         }))}
-                        value={this.props.address}
+                        value={this.state.address}
                         onChange={this.handleChange("address")}
                         placeholder={this.props.address ? this.props.address : "Enter location"}
                         onInputChange={this.handleAddress}
                     />
 
                 </div>
-                {this.props.address}
                 <button
                     className={'btn btn-primary'}
                     onClick={this.handleAddLocation}
