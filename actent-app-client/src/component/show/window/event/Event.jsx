cd53f79c7f23e43a30ca911b977e7ebd2baa21b3
Barnabass <rastafarimood@gmail.com>
2019-04-20 20:08:00 +0300

    Added image to event show page.

--- actent-app-client/src/component/show/window/event/Event.jsx
+++ actent-app-client/src/component/show/window/event/Event.jsx
@@ -1,18 +1,29 @@
 import React from 'react';
 import Description from './Description.jsx';
-import Image from './Image.jsx';
 import './Event.css';
 
 class Event extends React.Component {
 
     render() {
 
+        let image = undefined;
+
+        if  (this.props.image) {
+            image = this.props.image;
+        } else {
+            image = (
+                <div >
+                    <img src="https://www.kaskademusic.com/wp-content/uploads/2018/06/sunsoaked-2018.jpg" className='img-responsive my-pic'></img>
+                </div>
+            )
+        }
+
         return (
 
             <div className='event-container'>
 
                 <div className='box-1 box'>
-                    <Image image={this.props.image}/>
+                    {image}
                 </div>
 
                 <div className='box-2 box'>
