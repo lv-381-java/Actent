de143f9b7c61c3afec931c3af531566b0dbe4add
Barnabass <rastafarimood@gmail.com>
2019-04-23 15:33:56 +0300

    Refactoring.

--- src/main/java/com/softserve/actent/model/dto/converter/EventConverter.java
+++ src/main/java/com/softserve/actent/model/dto/converter/EventConverter.java
@@ -1,14 +1,12 @@
 package com.softserve.actent.model.dto.converter;
 
 import com.softserve.actent.constant.ExceptionMessages;
-import com.softserve.actent.constant.StringConstants;
 import com.softserve.actent.exceptions.DataNotFoundException;
 import com.softserve.actent.exceptions.codes.ExceptionCode;
 import com.softserve.actent.model.dto.TagDto;
 import com.softserve.actent.model.dto.equipment.EquipmentDto;
 import com.softserve.actent.model.dto.event.*;
 import com.softserve.actent.model.dto.eventUser.EventUserForEventDto;
-import com.softserve.actent.model.dto.review.ReviewDto;
 import com.softserve.actent.model.entity.*;
 
 import org.modelmapper.ModelMapper;
@@ -50,7 +48,7 @@ public class EventConverter {
         nullHunter(event, ExceptionMessages.EVENT_CAN_NOT_BE_NULL);
         return modelMapper.map(event, MinimalEventDto.class);
     }
-    private EventOstapDto getOstapDto(Event event) {
+    private EventOstapDto getForListDto(Event event) {
         nullHunter(event, ExceptionMessages.EVENT_CAN_NOT_BE_NULL);
         EventOstapDto eventOstapDto = modelMapper.map(event, EventOstapDto.class);
         eventOstapDto.setLocationForEventDto(getLocation(event.getAddress()));
@@ -169,7 +167,7 @@ public class EventConverter {
                 case MINIMAL:
                     return this::getMinimalEventDto;
                 case FOR_LIST:
-                    return this::getOstapDto;
+                    return this::getForListDto;
                 case DETAIL:
                     return this::getShowEventDto;
                 case FULL:
